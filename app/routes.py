d369f58888ab55c7b0eee5439552a478f738bfb3
Nikolay Ignatiev <naignatiev@yandex.ru>
2024-03-04 17:19:21 +0300

    Login form

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for, request
 
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -22,3 +23,15 @@ def index():
         }
     ]
     return render_template('index.html', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    if request.method == 'POST':
+        return 'LOX'
+    return render_template('login.html', title='Sign in', form=form)
---
+++ app/validators.py
@@ -0,0 +1,18 @@
+import re
+
+from wtforms.validators import Regexp
+
+
+class PasswordRestrictions:
+    def __init__(self):
+        self.message = ''
+        self._validators = [
+            Regexp(regex='.*[0-9].*', message='Password must contain digits'),
+            Regexp(regex='.*[a-zA-Z].*', message='Password must contain letter')
+        ]
+
+    def __call__(self, form, field, message=None):
+        for validator in self._validators:
+            validator(form, field)
+
+        return True
